07d80f5572270dc457026d6c76964e2960ed2837
Ivan Zotov <ivanzotov@gmail.com>
2016-06-02 23:47:10 +0300

    Remove connect from import and remove export for root

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,12 @@
-/**
- * ProFit
- * https://github.com/ivanzotov/ProFit
- * @flow
- */
-
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from './app/configureStore';
 import Main from './app/containers/main';
 
 const store = configureStore();
 
-export default class Root extends Component {
+class Root extends Component {
   render() {
     return (
       <Provider store={store}>
